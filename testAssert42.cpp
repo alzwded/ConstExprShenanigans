fdeae1c683da39b5f09c7f9fe9cd6c03ec1c2e36
Vlad <alzwded@gmail.com>
2016-04-10 16:00:37 +0300

    made it decode the error code into something human readable

--- testAssert42.cpp
+++ testAssert42.cpp
@@ -1,8 +1,25 @@
+#include <type_traits>
 enum class Code {
     OK = 0,
     MISSING,
     MALFORMED
 };
+
+struct E_OK { constexpr E_OK(){} constexpr operator bool() const { return true; }};
+struct E_MISSING { constexpr E_MISSING(){} constexpr operator bool() const { return false; }};
+struct E_MALFORMED { constexpr E_MALFORMED(){} constexpr operator bool() const { return false; }};
+
+template<Code C>
+struct decode;
+
+template<>
+struct decode<Code::OK> { static constexpr E_OK validity(){return{};} };
+template<>
+struct decode<Code::MISSING> { static constexpr E_MISSING validity(){return{};} };
+template<>
+struct decode<Code::MALFORMED> { static constexpr E_MALFORMED validity(){return{};} };
+
+
 
 struct Buf {
     Code const code;
@@ -17,10 +34,17 @@ struct Buf {
     constexpr int GetLine() const { return line; }
 };
 
-template<Code error, int line>
+template<typename error, int line>
+void check2()
+{
+    static_assert(error(), "Compilation Error");
+}
+
+template<typename error, int line>
 void check()
 {
-    static_assert(error == Code::OK, "Compilation failed");
+    auto constexpr error2 = error::validity();
+    check2<decltype(error2), line>();
 }
 
 int main()
@@ -28,8 +52,10 @@ int main()
     constexpr Buf a1(Code::OK, 10);
     constexpr Buf a2(Code::MISSING, 11);
     constexpr Buf a3(Code::MALFORMED, 12);
+    constexpr Buf a4(Code::MISSING, 23);
 
-    check<a1.GetCode(), a1.GetLine()>();
-    check<a2.GetCode(), a2.GetLine()>();
-    check<a3.GetCode(), a3.GetLine()>();
+    check<decode<a1.GetCode()>, a1.GetLine()>();
+    check<decode<a2.GetCode()>, a2.GetLine()>();
+    check<decode<a3.GetCode()>, a3.GetLine()>();
+    check<decode<a4.GetCode()>, a4.GetLine()>();
 }
