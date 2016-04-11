89a02856f66c002417fc580d0175731abb1a01d5
Vlad <alzwded@gmail.com>
2016-04-11 16:34:08 +0300

    pretty output

--- bits/test_rt.cpp
+++ bits/test_rt.cpp
@@ -2,39 +2,59 @@
 #include "validate.hpp"
 #include "parser_rt.hpp"
 #include <cstdio>
+#ifdef _WIN32
+# include <windows.h>
+#endif
 
 #ifndef TEST
 # error "Please specify a test."
 #endif
 
+#define TESTCASE(S, C, L)\
+    TinyBasicParser p(Buf(S));\
+    auto refCode = C;\
+    int refLine = L;\
+    auto rv = p.file();\
+    auto code = rv.code();\
+    auto line = rv.lineNo();\
+    auto source = p.buf().text()
+
 using namespace Jak;
 int main()
 {
 #if TEST == 1
-    TinyBasicParser p(Buf("10 PRINT 'Hello, World!'"));
-    int refCode = 0;
-    int refLine = 1;
+    TESTCASE("10 PRINT 'Hello, World!'", Code::Okay, 1);
 #elif TEST == 2
-    TinyBasicParser p(Buf("\
+    TESTCASE("\
 10 PRINT 'Hello, World!'\n\
-20 GOTO 10"));
-    int refCode = 0;
-    int refLine = 2;
+20 GOTO 10",
+    Code::Okay, 2);
 #elif TEST == 3
-    TinyBasicParser p(Buf("\
+    TESTCASE("\
 10 PRINT 'Hello, World!'\n\
-20 GOTO 10\n"));
-    int refCode = 0;
-    int refLine = 3;
+20 GOTO 10\n",
+    Code::Okay, 3);
 #elif TEST == 4
-    TinyBasicParser p(Buf("\
-10 PIRNT 'Hello, World!'"));
-    int refCode = 12;
-    int refLine = 1;
+    TESTCASE("\
+10 PIRNT 'Hello, World!'",
+    Code::UnknownKeyword, 1);
 #endif
-    auto rv = p.file();
-    auto code = static_cast<int>(rv.code());
-    auto line = rv.lineNo();
-    printf("%d %d\n", code, line);
-    printf("\n%s\n", (code == refCode && line == refLine) ? "PASS" : "FAIL");
+#ifdef PRINT_INFO
+    printf("source =\n%s$\n", source);
+    printf("code = %d line = %d\n", static_cast<int>(code), line);
+    printf("\n");
+#endif
+    auto pass = (code == refCode && line == refLine);
+#ifdef _WIN32
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    auto h = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(h, &csbi);
+    if(pass) SetConsoleTextAttribute(h, FOREGROUND_GREEN|FOREGROUND_INTENSITY);
+    else SetConsoleTextAttribute(h, FOREGROUND_RED|FOREGROUND_INTENSITY);
+#endif
+    printf("%s", pass ? "PASS" : "FAIL");
+#ifdef _WIN32
+    SetConsoleTextAttribute(h, csbi.wAttributes);
+#endif
+    printf("\n");
 }
--- testrt.bat
+++ testrt.bat
@@ -4,11 +4,22 @@ SETLOCAL
 IF #%1 == # GOTO :NOFILE
 SET testName=%1
 
-IF #%2 NEQ # GOTO :BADCANRY
+IF #%2 == # GOTO :EXEC
+IF #%2 == #noinfo GOTO :SILENT
+
+IF #%3 NEQ # GOTO :BADCANRY
+
+SET ADDOPTS=-DPRINT_INFO
+GOTO :EXEC
+
+:SILENT
+GOTO :EXEC
+
+:EXEC
 
 SET cppName=bits/test_rt.cpp
 
-g++ --std=gnu++14 -g -I. -I./bits -DTEST=%testName% %cppName% && a
+g++ --std=gnu++14 -g -I. -I./bits -DTEST=%testName% %ADDOPTS% %cppName% && a
 
 GOTO :EOF
 
