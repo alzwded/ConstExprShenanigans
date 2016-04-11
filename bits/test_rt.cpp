f93b0d995133bb7bb3c946667b1a1da8d5aca908
Vlad <alzwded@gmail.com>
2016-04-11 15:48:55 +0300

    also compare with ref line number

--- bits/test_rt.cpp
+++ bits/test_rt.cpp
@@ -13,22 +13,28 @@ int main()
 #if TEST == 1
     TinyBasicParser p(Buf("10 PRINT 'Hello, World!'"));
     int refCode = 0;
+    int refLine = 1;
 #elif TEST == 2
     TinyBasicParser p(Buf("\
 10 PRINT 'Hello, World!'\n\
 20 GOTO 10"));
     int refCode = 0;
+    int refLine = 2;
 #elif TEST == 3
     TinyBasicParser p(Buf("\
 10 PRINT 'Hello, World!'\n\
 20 GOTO 10\n"));
     int refCode = 0;
+    int refLine = 3;
 #elif TEST == 4
     TinyBasicParser p(Buf("\
 10 PIRNT 'Hello, World!'"));
     int refCode = 12;
+    int refLine = 1;
 #endif
-    auto code = static_cast<int>(p.file().code());
-    printf("%d\n", code);
-    printf("\n%s\n", (code == refCode) ? "PASS" : "FAIL");
+    auto rv = p.file();
+    auto code = static_cast<int>(rv.code());
+    auto line = rv.lineNo();
+    printf("%d %d\n", code, line);
+    printf("\n%s\n", (code == refCode && line == refLine) ? "PASS" : "FAIL");
 }
